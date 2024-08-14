f7f1b3c83d952ebcca07ce7553e7d43095db7ae9
Aman Kumar Srivastav <113780529+amansrivastavv@users.noreply.github.com>
2024-08-14 13:04:52 +0000

    Conversion of Data type

---
+++ Day1/conversionOperations.js
@@ -0,0 +1,16 @@
+let score = "20"
+console.log(typeof (score));
+
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber);
+
+
+// "20" => 20
+// "20abc" => NaN
+// true => 1; false =>0
+
+let isLoggedIn = "Aman"
+
+let booleanInLoggedIn = Boolean(isLoggedIn)
+console.log(valueInNumber);
