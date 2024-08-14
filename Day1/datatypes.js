26a19ed0322920d44a246bd45fe7874423009924
Aman Kumar Srivastav <113780529+amansrivastavv@users.noreply.github.com>
2024-08-14 11:35:42 +0000

    data types done

---
+++ Day1/datatypes.js
@@ -0,0 +1,17 @@
+let name ="Aman"
+age =18
+let isLoggedIn = false
+
+
+
+//number => 2 to power 53
+//bighint
+//string => ""
+//boolean => true/false
+//null=>standalone value
+//undefined=>
+//symbol => unique
+
+//object
+
+console.log(typeof "aman")
---
+++ Day1/variables.js
@@ -0,0 +1,7 @@
+const accountID = 151825
+let accountEmail ="aman@gmail.com"
+var accountPass ="123456"
+accountCity ="noida"
+
+console.log(accountID);
+console.table([accountID,accountEmail,accountPass,accountCity])
