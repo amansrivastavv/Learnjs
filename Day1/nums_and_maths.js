d41d4b467b39b7e18878d0de250495b53d57e895
Aman Kumar Srivastav <113780529+amansrivastavv@users.noreply.github.com>
2024-08-16 05:57:27 +0000

    Nums And Math done

---
+++ Day1/nums_and_maths.js
@@ -0,0 +1,41 @@
+// const score =400
+
+//  const Balance =new Number(100)
+//  console.log(Balance);
+//  console.log(score);
+//  console.log(Balance.toString().length);
+//  console.log(Balance.toFixed(2));
+
+
+//  const amount =10000000
+//  console.log(amount.toLocaleString('en-IN'));
+ 
+ //------------------------------/Maths/------------------------------
+ 
+
+//  console.log(Math);
+//  console.log(Math.abs(-4));
+//  console.log(Math.round(4.6));
+//  console.log(Math.round(4.3));
+//  console.log(Math.ceil(4.2));
+//  console.log(Math.floor(4.8));
+
+// console.log(Math.min(4,6,8,9));
+// console.log(Math.max(1,5,8,2,4))
+
+ 
+//  console.log(Math.random());
+//  console.log(Math.random()*10+1);
+
+ console.log(Math.floor(Math.random()*10)+1);
+
+ const min=10
+ const max=20
+
+ console.log(Math.floor(Math.random()*(max-min +1)) +min);
+ 
+ 
+
+ 
+ 
+ 
---
+++ Day1/strings.js
@@ -0,0 +1,14 @@
+const name = "Aman"
+const repoCount ="8"
+
+//console.log(name + repoCount +" value");
+
+//console.log(`hello my name is ${name} and my repoCount is ${repoCount}`);
+
+const gameName = new String('aman')
+
+console.log(gameName.length);
+console.log(gameName[1]);
+console.log(gameName.toUpperCase());
+console.log(gameName.indexOf('a'));
+console.log(gameName.charAt(3));
