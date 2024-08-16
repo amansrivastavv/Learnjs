696b901330258f92e36cfe5133cf8ebfce27960a
Aman Kumar Srivastav <113780529+amansrivastavv@users.noreply.github.com>
2024-08-16 08:35:30 +0000

    Dates in Js Done

---
+++ Day1/datesinjs.js
@@ -0,0 +1,22 @@
+//Dates
+// let myDate =new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleTimeString());
+
+// let myCreatedDate = new Date(2024, 4, 20)
+//  let myCreatedDate = new Date(2024, 7, 16, 13, 53)
+// // console.log(myCreatedDate.toLocaleString());
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.getMonth());
+console.log(myDate.getDay());
