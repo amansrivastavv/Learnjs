a15679169ec16da640012a19f5246b3501529d86
Aman Kumar Srivastav <113780529+amansrivastavv@users.noreply.github.com>
2024-08-23 10:39:42 +0000

    Array done

---
+++ Day2/array.js
@@ -0,0 +1,25 @@
+//  //array
+
+//  const myArr =[0,1,2,3,4,5]
+// //  const myHeros =["thor","batman"]
+// //  const myArr2= new Array(1,2,3,4)
+// //  console.log(myArr[4]);
+// //  console.log(myHeros[1]);
+// //  console.log(myArr2[3]);
+
+// // myArr.push(6,7)
+// // myArr.pop()
+
+// // myArr.unshift(2)
+// myArr.shift()
+// console.log(myArr);
+
+// let name ="Aman"
+// console.log(Array.from(name))
+// console.log(Array.isArray(name))
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1, score2,score3));
